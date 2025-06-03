a8dc2fde7c9731f7e1528c9fbead0dc07a8228f7
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-06-03 13:22:45 +0200

    Update App with new feature AddTodo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+import type { Todo } from './types';
+import AddTodo from './components/AddTodo';
+
 function App() {
-  return (
-    <div>
-      <h1>todos</h1>
-    </div>
-  )
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const handleAddTodo = (text: string) => {
+        const newTodo: Todo = {
+            id: crypto.randomUUID(),
+            text,
+            completed: false,
+        };
+        setTodos([...todos, newTodo]);
+    }
+
+    return (
+        <div>
+            <h1>todos</h1>
+            <AddTodo onAdd={handleAddTodo} />
+        </div>
+    )
 }
 
 export default App;
