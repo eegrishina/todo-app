f173567a7f01f104a4591b2b49b35d886a5175ae
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-06-03 17:02:02 +0200

    Update TodoOptions structure and add style

--- src/components/TodoOptions.tsx
+++ src/components/TodoOptions.tsx
@@ -6,21 +6,37 @@ interface TodoOptionsProps {
 }
 
 export default function TodoOptions({ incompleted, currentFilter, onChangeFilter, onClear }: TodoOptionsProps) {
+    const filters: Array<"all" | "active" | "completed"> = ["all", "active", "completed"];
+
     return (
-        <div>
-            <p>{incompleted} {incompleted === 1 ? "item" : "items"} left!</p>
-            <div>
-                <button onClick={() => onChangeFilter("all")}>
-                    All
-                </button>
-                <button onClick={() => onChangeFilter("active")}>
-                    Active
-                </button>
-                <button onClick={() => onChangeFilter("completed")}>
-                    Completed
-                </button>
+        <div className="flex justify-between items-center font-light text-gray-400 p-4">
+            <p className="text-green-700">
+                {incompleted} {incompleted === 1 ? "item" : "items"} left!
+            </p>
+
+            <div className="flex justify-between items-center gap-4">
+                {filters.map(filter => {
+                    const isActive = currentFilter === filter;
+                    return (
+                        <button
+                            key={filter}
+                            onClick={() => onChangeFilter(filter)}
+                            className={`px-3 py-1 rounded text-sm capitalize cursor-pointer select-none border border-white ${isActive
+                                ? "bg-rose-800 text-white border-rose-800"
+                                : "hover:border-rose-800 hover:text-gray-600"}`}
+                        >
+                            {filter}
+                        </button>
+                    )
+                })}
             </div>
-            <button onClick={onClear}>Clear complited</button>
+
+            <button
+                className="cursor-pointer select-none hover:text-gray-600"
+                onClick={onClear}
+            >
+                Clear complited
+            </button>
         </div>
     )
 }
