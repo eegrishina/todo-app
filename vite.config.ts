8ac7077093f9f7e096c77004936ec0f9c1871438
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-06-03 12:41:07 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/tests.ts',
+  },
+});
