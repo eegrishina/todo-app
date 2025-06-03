4ea4648004a00065109ea0589a6822e5e1754884
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-06-03 18:23:31 +0200

    Add test to filter tasks

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -10,3 +10,21 @@ test("User can add a new task", () => {
 
     expect(screen.getByText("Complete test task")).toBeInTheDocument();
 });
+
+test("Filter shows only active tasks", () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText(/What needs to be done/i);
+    fireEvent.change(input, { target: { value: "Done" } });
+    fireEvent.keyDown(input, { key: "Enter" });
+    fireEvent.change(input, { target: { value: "Todo" } });
+    fireEvent.keyDown(input, { key: "Enter" });
+
+    const checkboxes = screen.getAllByRole("checkbox");
+    fireEvent.click(checkboxes[0]);
+
+    fireEvent.click(screen.getByText(/active/i));
+
+    expect(screen.queryByText("Done")).not.toBeInTheDocument();
+    expect(screen.getByText("Todo")).toBeInTheDocument();
+});
