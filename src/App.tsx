3908943a028450e5519439ee7bb9100a2cf5708b
Ekaterina Grishina <124692458+eegrishina@users.noreply.github.com>
2025-06-03 13:50:19 +0200

    Add component AddTodo (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import type { Todo } from './types';
+import AddTodo from './components/AddTodo';
+
 function App() {
-  return (
-    <div>
-      <h1>todos</h1>
-    </div>
-  )
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const handleAddTodo = (text: string) => {
+        const newTodo: Todo = {
+            id: crypto.randomUUID(),
+            text,
+            completed: false,
+        };
+        setTodos([...todos, newTodo]);
+    }
+
+    return (
+        <div>
+            <h1 className="text-[100px] text-rose-800 font-light text-center opacity-18">todos</h1>
+            <div className="mx-auto max-w-xl shadow-md mt-10 bg-white">
+                <AddTodo onAdd={handleAddTodo} />
+            </div>
+        </div>
+    )
 }
 
 export default App;
