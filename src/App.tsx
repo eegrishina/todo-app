67413ce1635990dd5af2f3f5108a36ba67c03293
Ekaterina Grishina <grischina.kotya17@gmail.com>
2025-06-03 14:19:43 +0200

    Update App with new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import type { Todo } from './types';
 import AddTodo from './components/AddTodo';
+import TodoList from "./components/TodoList";
 
 function App() {
     const [todos, setTodos] = useState<Todo[]>([]);
@@ -12,13 +13,20 @@ function App() {
             completed: false,
         };
         setTodos([...todos, newTodo]);
-    }
+    };
+
+    const handleToggle = (id: string) => {
+        setTodos(prev => prev.map(todo =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        ))
+    };
 
     return (
         <div>
             <h1 className="text-[100px] text-rose-800 font-light text-center opacity-18">todos</h1>
             <div className="mx-auto max-w-xl shadow-md mt-10 bg-white">
                 <AddTodo onAdd={handleAddTodo} />
+                <TodoList list={todos} onToggle={handleToggle} />
             </div>
         </div>
     )
